b94e4fbee6f151963af65a480c85e99ae33d8fd5
enjoy-all-tiktok-vairal-video <rajame331@gmail.com>
2026-03-29 16:22:32 +0600

    Create database.js

---
+++ database.js
@@ -0,0 +1,28 @@
+// database.js ফাইলের ভেতরে শুধু এইটুকু থাকবে
+const videos = {
+    "0001075": { 
+        title: "Episode 199", 
+        image: "https://i.ibb.co/zVTgBQ79/poster.jpg", 
+        final: "https://t.me/enjoy420_bot?start=c6c1c72e", 
+        cat: "all", 
+        date: "30 Mar 2026", 
+        time: "03:45 PM" 
+    },
+    "0001095": { 
+        title: "Episode 198", 
+        image: "https://i.ibb.co/zVTgBQ79/poster.jpg", 
+        final: "https://t.me/vaiiralwathevideo_bot?start=c6c1c72e", 
+        cat: "all", 
+        date: "28 Mar 2026", 
+        time: "03:45 PM" 
+    },
+    "0001050": { 
+        title: "Episode 143", 
+        image: "https://i.ibb.co/8DsPZxpr/poster.jpg", 
+        final: "https://t.me/vaiiralwathevideo_bot?start=60579515", 
+        cat: "tiktok", 
+        date: "28 Mar 2026", 
+        time: "01:20 PM" 
+    }
+    // নতুন ভিডিও আসলে জাস্ট এই ফরম্যাটে নিচে নিচে যোগ করবেন
+};
